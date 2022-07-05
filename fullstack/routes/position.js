55c6705baeac2b67dd1a7242b574631fa72a10f7
Berezin Stepan <stepan-berezin@list.ru>
2022-07-05 18:01:51 +1000

    Создал дополнительные маршруты и контроллеры

---
+++ fullstack/routes/position.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const controllers = require('../controllers/position')
+const router = express.Router()
+
+
+// localhost:5000/api/auth/login
+router.get('/login',controllers.login)
+// localhost:5000/api/auth/register
+router.get('/register', controllers.register)
+
+module.exports = router
